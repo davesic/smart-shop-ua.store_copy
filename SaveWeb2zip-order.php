9bdfdeac5632cbae850c7fea6c25a20dc12876d3
davekamenenko <webdavee@gmail.com>
2023-02-10 16:18:46 +0200

    23

--- SaveWeb2zip-order.php
+++
@@ -1,7 +0,0 @@
-<?php
-    
-    $text = $_POST;
-    echo $text;
-    mail("WEBDAVEE@GMAIL.COM", "New Lead", $text, "X-Mailer: PHP/");
-    header('Location: confirm.html'); // ссылка на страницу спасибо
-    ?>
